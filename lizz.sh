972b270890195431b7d5a832f3671fc8b148d36b
pinkzz <jgdaflores184186@hotmail.com>
2022-11-25 19:07:20 -0300

    ---

--- lizz.sh
+++ lizz.sh
@@ -1,6 +1,19 @@
-#!/bin/bash
-source ./system/system/system.sh
+source ./.system/modules/system/system.sh
+source ./.system/modules/drivers/drivers.sh
 
+#dependency
 ! command -v dialog &> /dev/null && INSTALL_PACKAGE dialog "sudo apt-get install dialog"
-LIZZ_MENU
 
+if [[ ! -f  $HOME/.fonts/lizz.cache ]]
+then
+	#dependency
+	! command -v wget &> /dev/null && INSTALL_PACKAGE wget "sudo apt-get install wget"
+
+	#dependency
+	[[ ! -f ./cache/3270/3270.zip ]] && INSTALL_PACKAGE 3270_1 \
+	"wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/3270.zip -P ./cache/3270"
+	INSTALL_PACKAGE 3270_2 \
+		"unzip ./cache/3270/3270.zip -d $HOME/.fonts && touch $HOME/.fonts/lizz.cache && fc-cache -fv"
+fi
+
+LIZZ_MENU
